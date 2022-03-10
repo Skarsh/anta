0f076385a6ada37705f99037590de62100d99e33
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-03-10 21:57:42 +0100

    Initial commit

---
+++ src/debugger.rs
@@ -0,0 +1,63 @@
+
+use std::io;
+
+use nix::sys::ptrace;
+use nix::sys::wait::{waitpid};
+use nix::unistd::{Pid};
+
+pub struct Debugger {
+    prog_name: String,
+    pid: Pid,
+    running: bool
+}
+
+impl Debugger {
+    pub fn new(prog_name: String, pid: Pid) -> Self {
+        Self {prog_name, pid, running: true}
+    }
+
+    pub fn run(&self) {
+        println!("Started debugging process {}", self.pid);
+
+        waitpid(self.pid, None).unwrap();
+         
+        while self.running {
+            let mut line = String::new();
+            io::stdin()
+                .read_line(&mut line)
+                .expect("Failed to read line");
+            
+            self.handle_command(line);
+        }
+    }
+
+    pub fn continue_execution(&self) {
+        ptrace::cont(self.pid, None).expect("failed to coninute execution");
+        waitpid(self.pid, None).expect("failed to waidpid");
+    }
+
+    fn handle_command(&self, line: String) {
+        let mut args = line.split_whitespace();
+        let command = args.next().unwrap();        
+
+        if command == "continue" {
+            self.continue_execution();
+        } else {
+            eprintln!("Unknown command");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_handle_command() {
+        let dbg = Debugger::new(String::from(""), Pid::this());
+        let line = String::from("continue execution");
+        dbg.handle_command(line);
+
+        assert!(true);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+
+// std
+use std::env;
+use std::ffi::{CStr, CString};
+use std::process::exit;
+
+// 3rd party
+use nix::unistd::{execv, fork, ForkResult, Pid};
+use nix::sys::ptrace;
+use nix::sys::wait::{waitpid, WaitPidFlag};
+
+// own
+mod debugger;
+use crate::debugger::Debugger;
+
+// constants
+const PREFIX_PATH: &str = "/home/skarsh/dev/security/re/samples";
+
+fn execute_debugee(path: CString) {
+    
+    ptrace::traceme().unwrap();
+
+    let args = vec![CString::new("").unwrap()];
+    execv(&path, &args[..]).unwrap();
+}
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect(); 
+
+    if args.len() < 2 {
+        eprintln!("Program name not specified");
+        exit(-1);
+    }
+
+    let program_name = &args[1];
+    let path = format!("{}/{}", PREFIX_PATH, program_name);
+    let path = CString::new(&*path).expect("CString::new failed");
+
+    match unsafe {fork()} {
+        Ok(ForkResult::Child) => {
+            println!("child");
+            execute_debugee(path)
+        },
+        Ok(ForkResult::Parent {child}) => {
+            println!("parent");
+            let debugger = Debugger::new(program_name.to_owned(), child);
+            debugger.run();
+        },
+        Err(err) => {
+            panic!("[main] fork() failed: {}", err);
+        }
+    }
+}
