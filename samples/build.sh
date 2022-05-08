f706c46373f9e4e1c512761d700cf7d94d053244
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-05-08 20:32:20 +0200

    Update samples build.sh

--- samples/build.sh
+++ samples/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mkdir bin && nasm -f elf64 src/assembly/hello.asm -o bin/hello.o && ld bin/hello.o -o bin/hello
+
+mkdir -p -- "bin" && nasm -f elf64 ./src/assembly/hello.asm -o ./bin/hello.o && ld ./bin/hello.o -o ./bin/hello
