9c13e587a45527981899a965bfcb5fe024c73072
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-05-10 17:43:18 +0200

    Add simple c sample

--- build_samples.sh
+++ build_samples.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 
-mkdir -p -- "./samples/bin" && nasm -f elf64 ./samples/src/assembly/hello.asm -o ./samples/bin/hello.o && ld ./samples/bin/hello.o -o ./samples/bin/hello
+mkdir -p -- "./samples/bin"
+nasm -f elf64 ./samples/src/assembly/hello.asm -o ./samples/bin/hello.o && ld ./samples/bin/hello.o -o ./samples/bin/hello
+gcc ./samples/src/c/entry_point.c -o ./samples/bin/entry_point
