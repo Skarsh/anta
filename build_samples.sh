1eb6b53918d8864ef694fb8212145b1d7dd2fef7
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-08-24 20:30:46 +0200

    Add -g flag to the compilation command

--- build_samples.sh
+++ build_samples.sh
@@ -3,4 +3,4 @@
 
 mkdir -p -- "./samples/bin"
 nasm -f elf64 ./samples/src/assembly/hello.asm -o ./samples/bin/hello.o && ld ./samples/bin/hello.o -o ./samples/bin/hello
-gcc ./samples/src/c/entry_point.c -o ./samples/bin/entry_point
+gcc ./samples/src/c/entry_point.c -o ./samples/bin/entry_point -g
